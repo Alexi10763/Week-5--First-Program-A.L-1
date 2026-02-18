3f23e9bd7bf6420445dd27f75527b26f25b8a385
Alexi10763 <alexia.leal2006@gmail.com>
2026-02-18 18:12:19 +0000

    Kind of completed

---
+++ Insert-Name-Here.py
@@ -0,0 +1,19 @@
+
+name = "Alexia"
+num = "15"
+
+print("Printing is okay I guess") 
+
+print(name) #example 3?
+print("my name is", name) #Put a comma before the variable
+name = "Ale"
+print("My nickname is", name)
+
+print("What is your favorite animal?")
+animal=input #Don't really know
+print("I didn't know that", animal, "is your favorite animal")
+
+if num>10:
+    print("Congrats your number is greater than 10")
+else:
+    print("Sorry your number is not greater")
---
+++ partner2_2-18.py
@@ -0,0 +1,43 @@
+# 1. Print a message to the screen
+message = "Hello again" 
+print(message)
+
+# 2. Change the message to your own words and run again
+message= "Welcome back!"
+print(message)
+
+# 3. Print your favorite food
+
+print("Tacos")
+
+# 4. Try printing a number instead of text
+print("20")
+
+# 5. Create a variable that stores your name
+name = "Alexia"
+
+# 6. Print the variable
+print(name)
+
+# 7. Change the name inside the variable and run again
+name = "Alex"
+print(name)
+# 8. Create a variable with a number
+age = "19"
+
+# 9. Print the age
+print(age)
+# 10. Add 2 to the age and print the result
+result = "age+2"
+print(result)
+# 11. Ask the user for their name
+name= input("What is your name?")
+# 12. Print a greeting using their name
+print("Hello,", name)
+# 13. Ask the user for a number
+
+
+# 14. Print the number they typed
+
+
+# 15. Print a goodbye message
